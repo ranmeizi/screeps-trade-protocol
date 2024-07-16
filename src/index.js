054a81d0f53adda30e19d60866689f2a783e2ae2
boboan <360969885@qq.com>
2024-07-16 17:23:32 +0800

    feat: 代码框架

--- src/index.js
+++ src/index.js
@@ -1,87 +1,215 @@
-const Api = {
-    setBlackList() {
+const MEMORY_KEY = 'custom_market'
 
-    },
-    trans(sourceRoom, targetRoom) {
-        
-    },
-    on() {
-
-    },
-    onBreak() {
-
-    },
-    onError() {
-
-    },
-    onSuccess() {
-
-    },
-    reissue() {
-
-    },
-    call(){
-        // recive message or poll the connection
-    }
-}
-
-const Ctx = {
-    set() {
-
-    },
-    get() {
-        return Memory['__screep_trans__'] || {}
-    }
-}
-
-const Events={
-    BreakEvent:function({message}){},
-    ErrorEvent:function({message}){},
-    SuccEvent:function({message}){}
-}
-
-const utils={
-    dispatchEvent(event){
-
-    }
-}
-
-const connection={
-    connect(terminal){
-
-    }
-}
-
-const impls={
-    trans(sourceRoom, targetRoom){
-        // check room exist
-        if(!Game.rooms[sourceRoom]){
-            const evt = Events.ErrorEvent({
-                message:'room not exist,'
-            })
-            return utils.dispatchEvent(evt)
+function checkMemory() {
+    if (!Memory[MEMORY_KEY]) {
+        Memory[MEMORY_KEY] = {
+            lastIncomingId: '',
+            blackList: [],
+            terminals: {}
         }
-        // check terminal exist
-        if(!Game.rooms[sourceRoom].terminal){
-            const evt = Events.ErrorEvent({
-                message:'terminal not exist,'
-            })
-            return utils.dispatchEvent(evt)
+    }
+}
+
+/**
+ * @type {(Transaction&{description:Record<string,any>})[]}
+ */
+let letestMessage = []
+
+/**
+ * 获取最新的消息
+ */
+function getLetestMessage() {
+    if (Game.market.incomingTransactions.length === 0) {
+        return
+    }
+
+    const { lastIncomingId } = Memory[MEMORY_KEY]
+
+    for (let transaction of Game.market.incomingTransactions) {
+        if (transaction.transactionId === lastIncomingId) {
+            // 终止
+            break;
         }
 
-        const ctx = Ctx.get()
-        // check terminal busy
-        const terminal = Game.rooms[sourceRoom].terminal
-        if(ctx[terminal.id]){
-            const evt = Events.ErrorEvent({
-                message:'terminal is now in trans,'
-            })
-            return utils.dispatchEvent(evt)
+        const { description } = transaction
+
+        try {
+            letestMessage.push({ ...transaction, description: JSON.parse(description) })
+        } catch { }
+    }
+
+    // 更新最新id
+    Memory[MEMORY_KEY].lastIncomingId = Game.market.incomingTransactions[0].transactionId
+}
+
+function run() {
+    // 检查 Memory
+    checkMemory()
+
+    // 接受消息
+    getLetestMessage()
+
+    for (let [roomName, memory] of Object.entries(Memory[MEMORY_KEY].terminals)) {
+        const { status } = memory
+
+        const handler = handlers[status]
+
+        if (handler) {
+            handler(new TradeTerminal(roomName))
         }
-        
-        // try connect
-        connection.connect(terminal)
     }
 }
 
-module.exports = Api
+/**
+ * 
+ * @param {string} roomName 
+ */
+function select(roomName) {
+    const terminal = Game.rooms[roomName].terminal
+    const memory = Memory[MEMORY_KEY].terminals[roomName]
+    return {
+        terminal,
+        memory
+    }
+}
+
+/**
+ * 
+ * @param {TradeTerminalMemory} memory 
+ * @param {Status} status 
+ */
+function transStatus(memory, status) {
+    memory.status = status
+    memory.status_tick = Game.time
+}
+
+/**
+ * @type {Record<Status,(trade:TradeTerminal)=>void>}
+ */
+const handlers = {
+    LISTEN(trade) {
+        // 等待交易单信息
+        for (let transaction of letestMessage) {
+            if (!trade.memory.connection.roomName && transaction.to === trade.terminal.room.name) {
+                if (transaction.description.t === 'conn') {
+                    const resource = transaction.description.r
+
+                    // 发送 rules 列表
+                    // send
+
+                    // 改变状态为 WAIT_SEND
+                    transStatus(trade.memory,'WAIT_SEND')
+                }
+            }
+        }
+    },
+    WAIT_LIST(trade) {
+        // 等待交易单信息中的 list 数据 画图
+        for (let transaction of letestMessage) {
+            if (trade.memory.connection.roomName === transaction.from && transaction.to === trade.terminal.room.name) {
+                if (transaction.description.t === 'list') {
+                    const data = transaction.description.d
+
+                    // 画图
+                    // draw
+
+                    // 改变状态为 WAIT_TRADE
+                    transStatus(trade.memory,'WAIT_TRADE')
+                }
+            }
+        }
+    },
+    WAIT_TRADE(trade) {
+        // 检查 sendBuf 有没有内容
+        if(trade.memory.sendBuf){
+            // 发送
+            // send
+
+            transStatus(trade.memory,'WAIT_RECIEVE')
+        }
+    },
+    WAIT_SEND(trade) {
+         // 等待交易单信息中的 list 数据 画图
+         for (let transaction of letestMessage) {
+            if (trade.memory.connection.roomName === transaction.from && transaction.to === trade.terminal.room.name) {
+                if (transaction.description.t === 'list') {
+                    const data = transaction.description.d
+
+                    // 发资源吧
+                    // draw
+
+                    // 改变状态为 
+                    transStatus(trade.memory,'COMPLETE')
+                }
+            }
+        }
+    },
+    WAIT_RECIEVE(trade) {
+        // 等待交易单信息中的 list 数据 画图
+        for (let transaction of letestMessage) {
+            if (trade.memory.connection.roomName === transaction.from && transaction.to === trade.terminal.room.name) {
+                if (transaction.description.t === 'list') {
+                    const data = transaction.description.d
+
+                    // 发资源吧
+                    // draw
+
+                    // 改变状态为 COMPLETE
+                    transStatus(trade.memory,'COMPLETE')
+                }
+            }
+        }
+    },
+    COMPLETE(trade) {
+        // 收尾
+
+        transStatus(trade.memory,'LISTEN')
+    }
+}
+
+class TradeTerminal {
+
+    /**
+     * terminal
+     * @memberof TradeTerminal
+     * @type {StructureTerminal}
+     */
+    terminal
+
+    /**
+     * @memberof TradeTerminal
+     * @type {TradeTerminalMemory | undefined}
+     */
+    memory
+
+    constructor(roomName) {
+        const { terminal, memory } = select(roomName)
+
+        this.terminal = terminal
+        this.memory = memory
+    }
+
+    /**
+     * 创建连接
+     */
+    connect(roomName) {
+
+    }
+
+    /**
+     * 
+     */
+    trade() {
+        if (this.memory.status === 'WAIT_TRADE') {
+            // 发送
+        } else {
+            // 你在干森么？
+        }
+    }
+}
+
+module.exports = {
+    select,
+    TradeTerminal,
+    run
+}
---
+++ types/index.d.ts
@@ -0,0 +1,62 @@
+interface Memory {
+    custom_market: {
+        // 搜索的上一条 incomingTransaction id
+        lastIncomingId: string,
+        blackList: string[],
+        terminals: Record<string, TradeTerminalMemory>
+    }
+}
+
+type Trade = {
+
+}
+
+type Status = 'LISTEN' | 'WAIT_LIST' | 'WAIT_TRADE' | 'WAIT_SEND' | 'WAIT_RECIEVE' | 'COMPLETE'
+
+type Rule = {
+    id: '',
+    type: string,
+    /**
+     * 每一定量商品需要的等价物数量
+     * 商品 type ， 数量 ，等价物 type ，数量
+     */
+    raito: [string, number, string, number]
+}
+
+type TradeTerminalMemory = {
+    /**
+     * 状态
+     */
+    status: Status
+
+    /**
+     * 状态改变tick
+     */
+    status_tick:number
+    /**
+     * 商品交换规则
+     */
+    rules: Rule[]
+    /**
+     * 发送缓冲区
+     */
+    sendBuf?:string
+
+    connection?:{
+        roomName:string
+    }
+}
+
+interface ITradeTerminal {
+    /**
+     * 创建连接
+     */
+    connect(roomName: string): void
+
+    /**
+     * 设置规则
+     * 
+     * 修改 memory rules
+     */
+    setRules(): void
+}
